e9685c8edaebacf69c7795c52d1b6a72bf100f7f
ehsan <eeboking1998@gmail.com>
2024-08-03 22:28:37 +0330

    DEV: 1- some mappers fixed.

--- src/main/java/com/sadri/bms/common/dto/user/UserOut.java
+++ src/main/java/com/sadri/bms/common/dto/user/UserOut.java
@@ -3,9 +3,11 @@ package com.sadri.bms.common.dto.user;
 import com.sadri.bms.common.dto.account.AccountOut;
 import com.sadri.bms.model.entity.UsersEntity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.Hibernate;
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class UserOut extends UserIn {
@@ -14,9 +16,6 @@ public class UserOut extends UserIn {
     private Long accountId;
     private String fullName;
     private AccountOut account;
-
-    public UserOut() {
-    }
 
     public UserOut(UsersEntity entity) {
         this.id = entity.getId();
@@ -28,7 +27,7 @@ public class UserOut extends UserIn {
         this.setBirthDate(entity.getBirthDate());
 
         if (Hibernate.isInitialized(entity.getAccount()) && entity.getAccount() != null) {
-            //account mapper
+            account = new AccountOut(entity.getAccount());
         }
     }
 }
